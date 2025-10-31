4ffb58fea190b5978755422cade07286b9ac2c6f
kunlulu <59993024+kunlulu@users.noreply.github.com>
2025-10-31 18:09:19 +0800

    fix: login page disable auto redirect  (#848)
    * fix: login redirect
    
    * fix
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: kunlulu <kunlulu810@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/cook-web/src/app/login/page.tsx
+++ src/cook-web/src/app/login/page.tsx
@@ -234,16 +234,16 @@ export default function AuthPage() {
     }
   }, [language, ready]);
 
-  useEffect(() => {
-    if (!isInitialized || !isLoggedIn) {
-      return;
-    }
+  // useEffect(() => {
+  //   if (!isInitialized || !isLoggedIn) {
+  //     return;
+  //   }
 
-    const target = resolveRedirectPath();
-    if (window.location.pathname !== target) {
-      router.replace(target);
-    }
-  }, [isInitialized, isLoggedIn, resolveRedirectPath, router]);
+  // const target = resolveRedirectPath();
+  // if (window.location.pathname !== target) {
+  //   router.replace(target);
+  // }
+  // }, [isInitialized, isLoggedIn, resolveRedirectPath, router]);
 
   const [googleTermsAccepted, setGoogleTermsAccepted] = useState(false);
   const [isGoogleLoading, setIsGoogleLoading] = useState(false);
