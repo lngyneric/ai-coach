44bf8edb0e6bc9998b0104a0be2285e4840de56f
sysmex <sysmex@localhost>
2026-06-14 11:16:19 +0000

    fix: nginx entrypoint cp + OPENCODE_API_KEY + certs2

--- docker/nginx-entrypoint.sh
+++ docker/nginx-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-# entrypoint for nginx: substitute ${ARK_API_KEY} in nginx.conf before starting
+# entrypoint for nginx: substitute env vars in nginx configs before starting
 export ARK_API_KEY="${ARK_API_KEY:-}"
-envsubst '${ARK_API_KEY}' < /etc/nginx/nginx.conf > /tmp/nginx.conf && \
-  mv /tmp/nginx.conf /etc/nginx/nginx.conf && \
+export OPENCODE_API_KEY="${OPENCODE_API_KEY:-${OPENAI_API_KEY:-}}"
+envsubst '${ARK_API_KEY} ${OPENCODE_API_KEY}' < /etc/nginx/nginx.conf > /tmp/nginx.conf && \
+  cp /tmp/nginx.conf /etc/nginx/nginx.conf && \
+  envsubst '${OPENCODE_API_KEY}' < /etc/nginx/conf.d/default.conf > /tmp/default.conf && \
+  cp /tmp/default.conf /etc/nginx/conf.d/default.conf && \
   exec nginx -g 'daemon off;'
